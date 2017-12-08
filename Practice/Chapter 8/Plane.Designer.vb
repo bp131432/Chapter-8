e0246ff0653de41982c82637f3fde7758b363a78
Bailey <33033378+bp131432@users.noreply.github.com>
2017-12-08 11:34:17 -0600

    update

---
+++ Practice/Chapter 8/Plane.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Plane
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Plane))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtRow = New System.Windows.Forms.TextBox()
+        Me.txtColumn = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.btnDisplayPrice = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(85, 115)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(303, 319)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter a row:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Enter a column:"
+        '
+        'txtRow
+        '
+        Me.txtRow.Location = New System.Drawing.Point(105, 27)
+        Me.txtRow.Name = "txtRow"
+        Me.txtRow.Size = New System.Drawing.Size(100, 20)
+        Me.txtRow.TabIndex = 3
+        '
+        'txtColumn
+        '
+        Me.txtColumn.Location = New System.Drawing.Point(105, 69)
+        Me.txtColumn.Name = "txtColumn"
+        Me.txtColumn.Size = New System.Drawing.Size(100, 20)
+        Me.txtColumn.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(289, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Seat Price:"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPrice.Location = New System.Drawing.Point(354, 52)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(100, 23)
+        Me.lblPrice.TabIndex = 6
+        '
+        'btnDisplayPrice
+        '
+        Me.btnDisplayPrice.Location = New System.Drawing.Point(85, 457)
+        Me.btnDisplayPrice.Name = "btnDisplayPrice"
+        Me.btnDisplayPrice.Size = New System.Drawing.Size(77, 39)
+        Me.btnDisplayPrice.TabIndex = 7
+        Me.btnDisplayPrice.Text = "Display Price"
+        Me.btnDisplayPrice.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(313, 457)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 39)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Plane
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(482, 508)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplayPrice)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtColumn)
+        Me.Controls.Add(Me.txtRow)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Plane"
+        Me.Text = "Plane"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtRow As System.Windows.Forms.TextBox
+    Friend WithEvents txtColumn As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblPrice As System.Windows.Forms.Label
+    Friend WithEvents btnDisplayPrice As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+End Class
---
+++ Practice/Chapter 8/Plane.vb
@@ -0,0 +1,36 @@
+﻿Public Class Plane
+
+    Private Sub btnDisplayPrice_Click(sender As Object, e As EventArgs) Handles btnDisplayPrice.Click
+        Dim intRow, intCol As Integer
+
+        Const intMAX_ROW As Integer = 5
+        Const intMAX_Col As Integer = 3
+
+        Dim decPrice(,) = {{450D, 450D, 450D, 450D},
+                           {425D, 425D, 425D, 425D},
+                           {400D, 400D, 400D, 400D},
+                           {375D, 375D, 375D, 375D},
+                           {375D, 375D, 375D, 375D},
+                           {350D, 350D, 350D, 350D}}
+        Try
+            intRow = CInt(txtRow.Text)
+            intCol = CInt(txtColumn.Text)
+
+            If intRow >= 0 And intRow <= intMAX_ROW Then
+                If intCol >= 0 And intCol <= intMAX_Col Then
+                    lblPrice.Text = decPrice(intRow, intCol).ToString("c")
+                Else
+                    MessageBox.Show("Column must be 0 through" & intMAX_Col.ToString())
+                End If
+            Else
+                MessageBox.Show("Row and columns must be integers")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
