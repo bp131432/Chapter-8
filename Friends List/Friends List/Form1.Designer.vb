510af93c1db01d655c7c050a92449acbe8337814
Bailey <33033378+bp131432@users.noreply.github.com>
2017-12-13 10:28:25 -0600

    Created friends

---
+++ Friends List/Friends List/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstFriends = New System.Windows.Forms.ListBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstFriends
+        '
+        Me.lstFriends.FormattingEnabled = True
+        Me.lstFriends.Location = New System.Drawing.Point(73, 44)
+        Me.lstFriends.Name = "lstFriends"
+        Me.lstFriends.Size = New System.Drawing.Size(181, 147)
+        Me.lstFriends.TabIndex = 0
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(42, 235)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 1
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(241, 235)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(357, 342)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lstFriends)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstFriends As System.Windows.Forms.ListBox
+    Friend WithEvents btnEnter As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Friends List/Friends List/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Const intMax_SUBSCRIPT As Integer = 4
+        Dim strNames(intMax_SUBSCRIPT) As String
+        Dim intCount As Integer
+
+        MessageBox.Show("I'm going to ask you to enter the names of five friends.")
+
+        For intCount = 0 To intMax_SUBSCRIPT
+            strNames(intCount) = InputBox("Enter a friends name")
+        Next
+        lstFriends.Items.Add(strNames(intCount))
+
+        For intCount = 0 To intMax_SUBSCRIPT
+            lstFriends.Items.Add(strNames(intCount))
+        Next
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
